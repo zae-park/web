475c3d0744e79453670bf59110ecdee867bda388
zae-park <zae_park@huinno.com>
2022-11-08 23:44:20 +0900

    add new js file.

---
+++ javascript/buttons.js
@@ -0,0 +1,11 @@
+white_btn = document.getElementById('white_btn');
+white_btn.addEventListener('click', function(){
+    document.getElementById('target').className='white';
+}
+)
+
+black_btn = document.getElementById('black_btn');
+black_btn.addEventListener('click', function(){
+    document.getElementById('target').className='black';
+}
+)
