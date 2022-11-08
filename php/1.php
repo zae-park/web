381dc7842a0eeb2bbdf6d35796d8b72a6a1dd688
zae-park <zae_park@huinno.com>
2022-11-08 23:34:14 +0900

    add color button

---
+++ php/1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>Javescript</h1>
+        <script charset="utf-8">
+            password = prompt("Password?");
+            name = "zae";
+            document.write("Welcome. " + name + "!")
+        </script>
+
+        <h1>PHP</h1>
+        <?php
+            $name = "zae";
+            echo "Welcome again." .+ $name .+ "!"
+        ?>
+        
+    </body>
+</html>
